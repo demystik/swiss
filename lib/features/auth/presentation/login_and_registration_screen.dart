3cd105c0a1787475b276b9bb762a7e1f5d76ab4f
demysti <demystiktech@gmail.com>
2026-07-10 17:45:10 +0100

    getting ended

--- lib/features/auth/presentation/login_and_registration_screen.dart
+++ lib/features/auth/presentation/login_and_registration_screen.dart
@@ -43,7 +43,6 @@ class _AuthTabBarViewState extends State<AuthTabBarView> {
   final _loginPasswordCtrl = TextEditingController();
   final _emailCtrl = TextEditingController();
   final _passwordCtrl = TextEditingController();
-  final _confirmPasswordCtrl = TextEditingController();
 
   @override
   void dispose() {
